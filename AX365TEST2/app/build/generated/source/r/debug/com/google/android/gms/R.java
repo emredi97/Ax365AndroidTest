7728b1d9544d23fe77e7f2315bb9791183a23c7b
emrehagi1997@gmail.com <emrehagi1997@gmail.com@074ac6ac-7e9c-11e7-9c42-3f18bda31295>
2017-12-27 09:19:24 +0000

    V4 100% working

--- AX365TEST2/app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ AX365TEST2/app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -93,9 +93,9 @@ public final class R {
         public static final int normal = 0x7f0d001a;
         public static final int radio = 0x7f0d006c;
         public static final int standard = 0x7f0d004a;
-        public static final int text = 0x7f0d00bf;
-        public static final int text2 = 0x7f0d00bd;
-        public static final int toolbar = 0x7f0d008b;
+        public static final int text = 0x7f0d00c0;
+        public static final int text2 = 0x7f0d00be;
+        public static final int toolbar = 0x7f0d008c;
         public static final int wide = 0x7f0d004b;
         public static final int wrap_content = 0x7f0d002d;
     }
