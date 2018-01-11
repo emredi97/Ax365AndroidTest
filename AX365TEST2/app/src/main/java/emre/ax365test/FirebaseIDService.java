7c3cceec7d037def127c1bea55d31e220d17009d
emrehagi1997@gmail.com <emrehagi1997@gmail.com@074ac6ac-7e9c-11e7-9c42-3f18bda31295>
2018-01-11 10:15:09 +0000

    11.01.2018

---
+++ AX365TEST2/app/src/main/java/emre/ax365test/FirebaseIDService.java
@@ -0,0 +1,36 @@
+package emre.ax365test;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by emreh_000 on 27.12.2017.
+ */
+
+public class FirebaseIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "FirebaseIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // Add custom implementation, as needed.
+    }
+}
